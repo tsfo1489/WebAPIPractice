e63b2b54decd0c0daf34256561ab97058a061c5a
Arepo <tsfo1489@gmail.com>
2021-06-28 16:06:02 +0900

    Use tweepy

--- Twitter/twitter.py
+++ Twitter/twitter.py
@@ -1,12 +1,22 @@
+from requests.sessions import session
 import tweepy
+from tweepy import api
+import apikey
+import datetime
 
+def time_format(date : datetime.datetime) :
+    return date.strftime('%Y-%m-%d')
 
+auth = tweepy.OAuthHandler(apikey.consumer_key, apikey.consumer_secret)
 
-api = twitter.Api(consumer_key=['mlIlDkXPFGsP0tAakd0opBQuJ'],
-                  consumer_secret=['KsfFiZahmfgVY1qo3g0dKjlOA9ZGdxQYXtUShIaimiCXKuamCH'],
-                  access_token_key=['1409319150137212928-kHr9zYzeYJi0n1s9lRNGaOTmkQWzMs'],
-                  access_token_secret=['ya6zk5B2epswthBhOyf2l7aCWUZ92lcWPvKiKd7e3ePvk'])
-results = api.GetSearch(
-    raw_query="q=twitter%20&result_type=recent&since=2014-07-19&count=100")
+auth.set_access_token(apikey.access_token_key, apikey.access_token_secret)
 
-print(results)
+api = tweepy.API(auth)
+today_date = datetime.datetime.now()
+
+for i in range(7) :
+    date = time_format(today_date - datetime.timedelta(days=i))
+    results = api.search_tweets('점심', lang='ko', count=100, until=date)
+    print(date, len(results))
+    if len(results) != 0 :
+        print(results[0].text)
