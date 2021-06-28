e8853f216864d9a3983e617bad56f72b08203da6
Arepo <tsfo1489@gmail.com>
2021-06-28 16:32:58 +0900

    Update for keyword Search

--- Twitter/twitter.py
+++ Twitter/twitter.py
@@ -13,10 +13,11 @@ auth.set_access_token(apikey.access_token_key, apikey.access_token_secret)
 
 api = tweepy.API(auth)
 today_date = datetime.datetime.now()
+keyword = 'Tag'
 
 for i in range(7) :
     date = time_format(today_date - datetime.timedelta(days=i))
-    results = api.search_tweets('점심', lang='ko', count=100, until=date)
+    results = api.search_tweets(keyword, lang='ko', count=100, until=date)
     print(date, len(results))
     if len(results) != 0 :
-        print(results[0].text)
+        print(results[0], results[0].text)
