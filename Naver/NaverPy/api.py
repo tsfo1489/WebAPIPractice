db90f225c79370ae441409fd8781c580d16979a4
Arepo <tsfo1489@gmail.com>
2021-06-30 10:14:41 +0900

    Add News and Book api

--- Naver/NaverPy/api.py
+++ Naver/NaverPy/api.py
@@ -20,10 +20,19 @@ class API :
         if method == 'GET' :
             return requests.get(endpoint, params=params, headers=hds)
             
+    def xml_to_json(self, xml : dict) -> dict :
+        temp_json = {}
+        raw_xml = xml['rss']['channel']
+        temp_json['lastBuildDate'] = raw_xml['lastBuildDate']
+        temp_json['total'] = int(raw_xml['total'])
+        temp_json['start'] = int(raw_xml['start'])
+        temp_json['display'] = int(raw_xml['display'])
+        temp_json['items'] = raw_xml['item']
+        return temp_json
         
     def search_blog(
         self, query : str, display=10, start=1, sort='sim', ext='json'
-        ) :
+    ) :
         url = self.mainURL + 'search/blog.'
     
         if ext == 'json' :
@@ -51,7 +60,69 @@ class API :
         if ext == 'json' :
             return json.loads(res.text)
         else :
-            return json.loads(json.dumps(xmltodict.parse(res.text), ensure_ascii=False))
+            return self.xml_to_json(xmltodict.parse(res.text))
 
+    def search_news(
+        self, query : str, display=10, start=1, sort='sim', ext='json'
+    ) :
+        url = self.mainURL + 'search/news.'
+    
+        if ext == 'json' :
+            url += 'json'
+        elif ext == 'xml' :
+            url += 'xml'
+        else :
+            raise ArgumentError('\'ext\' must be json or xml')
 
-        
+        if display > 100 or display < 1:
+            raise ArgumentError('\'display\' arg must be between 1 and 100')
+        if start < 1 or start > 1000 :
+            raise ArgumentError('\'start\' arg must be between 1 and 1000')
+        if not(sort == 'sim' or sort == 'date') :
+            raise ArgumentError('\'sort\' arg must be sim or date')
+
+        param = {
+            'query' : query,
+            'display' : display,
+            'start' : start,
+            'sort' : sort
+        }
+        res = self.request('GET', url, param)
+
+        if ext == 'json' :
+            return json.loads(res.text)
+        else :
+            return self.xml_to_json(xmltodict.parse(res.text))
+
+            
+    def search_book(
+        self, query : str, display=10, start=1, sort='sim', ext='json'
+    ) :
+        url = self.mainURL + 'search/book.'
+    
+        if ext == 'json' :
+            url += 'json'
+        elif ext == 'xml' :
+            url += 'xml'
+        else :
+            raise ArgumentError('\'ext\' must be json or xml')
+
+        if display > 100 or display < 1:
+            raise ArgumentError('\'display\' arg must be between 1 and 100')
+        if start < 1 or start > 1000 :
+            raise ArgumentError('\'start\' arg must be between 1 and 1000')
+        if not(sort == 'sim' or sort == 'date') :
+            raise ArgumentError('\'sort\' arg must be sim or date')
+
+        param = {
+            'query' : query,
+            'display' : display,
+            'start' : start,
+            'sort' : sort
+        }
+        res = self.request('GET', url, param)
+
+        if ext == 'json' :
+            return json.loads(res.text)
+        else :
+            return self.xml_to_json(xmltodict.parse(res.text))
--- Naver/test.py
+++ Naver/test.py
@@ -4,8 +4,7 @@ import json
 
 api = API(apikey.client_ID, apikey.client_secret)
 
-f_xml = open('xml.json', 'w', encoding='utf-8')
-f_xml.write(json.dumps(api.search_blog('Test', ext='xml'), ensure_ascii=False))
+test_keyword = '대선'
 
 f_jso = open('json.json', 'w', encoding='utf-8')
-f_jso.write(json.dumps(api.search_blog('Test', ext='json'), ensure_ascii=False))
+f_jso.write(json.dumps(api.search_book(test_keyword, display=100, ext='json'), ensure_ascii=False))
